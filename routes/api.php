03244d6c23fe615c037e8e5a0143ca3260454bf4
Victor73i <deleonvictor839@gmail.com>
2021-06-13 12:10:40 -0600

    we made the routes of web.php and the routes of api.php

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('get-estudiante', ['\App\Http\Controllers\EstudianteController', 'getAll'])->name('api-getAll');
+Route::put('save-estudiante', ['\App\Http\Controllers\EstudianteController', 'saveEstudiante'])->name('api-saveEstudiante');
+Route::delete('delete-estudiante/{id}', ['\App\Http\Controllers\EstudianteController', 'deleteEstudiante2'])->name('api-deleteEstudiante2');
+Route::post('edit-estudiante/{id}', ['\App\Http\Controllers\EstudianteController', 'editEstudiante2'])->name('api-editEstudiante');
+Route::get('show-estudiante', ['\App\Http\Controllers\EstudianteController', 'showEstudiante'])->name('api-showEstudiante');
+
+Route::get('get-genero', ['\App\Http\Controllers\GeneroController', 'getAll'])->name('api-getAll');
+Route::put('save-genero', ['\App\Http\Controllers\GeneroController', 'saveGenero'])->name('api-saveGenero');
+Route::delete('delete-genero/{id}', ['\App\Http\Controllers\GeneroController', 'deleteGenero2'])->name('api-deleteGenero2');
+Route::post('edit-genero/{id}', ['\App\Http\Controllers\GeneroController', 'editGenero2'])->name('api-editGenero');
+Route::get('show-genero', ['\App\Http\Controllers\GeneroController', 'showGenero'])->name('api-showGenero');
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/estudiantes/index', [\App\Http\Controllers\EstudianteController::class, 'registerEstudiante'])->name('estudiante.index');
+Route::post('/estudiantes/registrar', [\App\Http\Controllers\EstudianteController::class, 'saveEstudiante'])->name('estudiante.registrarEstudiante');
+Route::get('/estudiantes/lista', [\App\Http\Controllers\EstudianteController::class, 'showEstudiante'])->name('estudiante.listarEstudiante');
+Route::get('/estudiantes/edit/{id}',[\App\Http\Controllers\EstudianteController::class,'edit'])->name('estudiante.editEstudiante');
+Route::post('/estudiantes/update/{id}',[\App\Http\Controllers\EstudianteController::class,'update'])->name('estudiante.updateEstudiante');
+Route::get('/estudiantes/delete/{id}',[\App\Http\Controllers\EstudianteController::class,'deleteEstudiante'])->name('estudiante.deleteEstudiante');
+Route::get('/generos/index', [\App\Http\Controllers\GeneroController::class, 'registerGenero'])->name('genero.index');
+Route::post('/generos/registrar', [\App\Http\Controllers\GeneroController::class, 'saveGenero'])->name('genero.registrarGenero');
+Route::get('/generos/lista', [\App\Http\Controllers\GeneroController::class, 'showGenero'])->name('genero.listarGenero');
+Route::get('/generos/edit/{id}',[\App\Http\Controllers\GeneroController::class,'edit'])->name('genero.editGenero');
+Route::post('/generos/update/{id}',[\App\Http\Controllers\GeneroController::class,'update'])->name('genero.updateGenero');
+Route::get('/generos/delete/{id}',[\App\Http\Controllers\GeneroController::class,'deleteGenero'])->name('genero.deleteGenero');
